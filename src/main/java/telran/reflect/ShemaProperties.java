fac18b0952d1c26fa80b1138b67ffae2a277dcb3
AlGuron <blizgur8@gmail.com>
2024-01-19 21:24:34 +0200

    ref

--- src/main/java/telran/reflect/ShemaProperties.java
+++ src/main/java/telran/reflect/ShemaProperties.java
@@ -2,26 +2,29 @@ package telran.reflect;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ShemaProperties {
 	public static void displayFieldProperties(Object obj) throws Exception {
-		Field[] fields = obj.getClass().getDeclaredFields();
-		ArrayList<Field> idArr = new ArrayList<Field>(0);
-		for (Field field : fields) {
-			if (field.isAnnotationPresent(Index.class)) {
-				System.out.println("annotation @Index at field: " + field.getName());
-			}
+		Field idField = null;
+		List<Field> indexFields = new ArrayList<>();
+		for (Field field : obj.getClass().getDeclaredFields()) {
 			if (field.isAnnotationPresent(Id.class)) {
-				idArr.add(field);
+				if (idField != null) {
+					throw new IllegalStateException("Field Id must be one");
+				}
+				idField = field;
+			}
+			if (field.isAnnotationPresent(Index.class)) {
+				indexFields.add(field);
 			}
 		}
-
-		if (idArr.isEmpty()) {
+		if (idField == null) {
 			throw new IllegalStateException("No field Id found");
 		}
-		if (idArr.size() > 1) {
-			throw new IllegalStateException("Field Id must be one");
-		}
-		System.out.println("annotation @Id at field: " + idArr.get(0).getName());
+		System.out.println("id field is " + idField.getName());
+		System.out.println("index field follow:");
+		indexFields.forEach(field -> System.out.print(field.getName() + " "));
+		System.out.println("\n");
 	}
 }
